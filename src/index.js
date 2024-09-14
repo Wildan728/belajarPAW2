94c6b0f0c8f280b5771d9a55d70acacd7dcf5a14
Wildan <160817910+Wildan728@users.noreply.github.com>
2024-09-14 09:26:39 +0700

    Perbaikan

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Example app listening on port 3000')
 })
+
+app.get('/about', (req, res) => {
+    res.send('Halaman About')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Halaman Contact')
+})
